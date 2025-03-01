175b4ebda9b44985ceca4fa73e8560ab9bcf6ad8
Fernando Borlone <fernandoborlone@Fernandos-MacBook-Pro.local>
2025-03-01 10:47:00 -0300

    add more locators

--- cypress/support/GUI/locators/locators.js
+++ cypress/support/GUI/locators/locators.js
@@ -1,5 +1,6 @@
 const locators = {
   PAGE_CONTENT: 'div.page-content',
+  HEADER_MENU: '#primary-site-navigation-desktop',
   SEARCH_BUTTON: 'div[class="ast-search-icon"]',
   SEARCH_FIELD: '#search-field',
   SEARCH_RESULTS: 'h1.page-title.ast-archive-title',
