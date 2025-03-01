5b5e0afbd4b2539d82731197de63f4e1c11d967b
Fernando Borlone <fernandoborlone@Fernandos-MacBook-Pro.local>
2025-03-01 05:55:39 -0300

    update cypress.config file

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -16,13 +16,13 @@ Cypress.Commands.add('wait_page_render', aliases => {
   })
 
   Cypress.Commands.add('verify_result_content', searchTerm => {
-    cy.wait_page_render('@postCollect')
+    cy.wait_page_render(3000)
     cy.contains(locators.SEARCH_RESULTS, 'Agibank').should('exist');
     cy.get(locators.SEARCH_RESULTS_CONTENT).first().click();
     cy.contains('#content', searchTerm)
 })
 
 Cypress.Commands.add('verify_message_result', messageResult => {
-    cy.wait_page_render('@postCollect')
+    cy.wait_page_render(3000)
     cy.get(locators.PAGE_CONTENT).should('contain', messageResult)
 })
